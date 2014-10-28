e11bdf380bc05b8e431ae0eec3b1e58d032f09fa
james mintram <james@lemonmoosegames.com>
2014-10-28 01:39:55 +0000

    Reinstated more code

--- cpp/console/tests.cpp
+++ cpp/console/tests.cpp
@@ -1,9 +1,11 @@
 #include <unistd.h>
-#include <pty.h>
+//#include <pty.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <errno.h>
+
+#include <util.h>
 
 #include "../console/buf.h"
 #include "../console/draw.h"
